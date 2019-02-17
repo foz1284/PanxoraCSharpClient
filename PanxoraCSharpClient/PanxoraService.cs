8c5972f6972c45c299cfb4b6dedf3b58a01a78bb
Mark Forster <foz1284@gmail.com>
2019-02-17 10:13:55 +0000

    Added API Methods

--- PanxoraCSharpClient/PanxoraService.cs
+++ PanxoraCSharpClient/PanxoraService.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using PanxoraCSharpClient.Models;
 using RestSharp;
+using System;
 using System.Collections.Generic;
 
 namespace PanxoraCSharpClient
@@ -46,6 +47,37 @@ namespace PanxoraCSharpClient
 
             return CreateCashOrder(cashOrderRequest);
         }
+        
+
+        public AccountSummaryDTO GetAccountSummary()
+        {
+            var request = new RestRequest("account-summary", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<AccountSummaryDTO>(response.Content);
+        }
+
+        public CashMovementDTO ListCashMovements(DateTime? startDate, DateTime? endDate)
+        {
+            var request = new RestRequest("cash-movements", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            if (startDate != null)
+            {
+                request.AddHeader("start", startDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+            if (endDate != null)
+            {
+                request.AddHeader("end", endDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<CashMovementDTO>(response.Content);
+        }
 
         public List<CashBalanceDTO> GetCashBalances()
         {
@@ -55,14 +87,24 @@ namespace PanxoraCSharpClient
 
             return JsonConvert.DeserializeObject<List<CashBalanceDTO>>(response.Content);
         }
-
-        public PricingDTO GetMarketDetails(string market)
+        
+        public IEnumerable<OrderDTO> ListCashOrders(DateTime? startDate, DateTime? endDate)
         {
-            var request = new RestRequest(string.Format("markets/{0}/prices", market), Method.GET);
+            var request = new RestRequest("cash-orders", Method.GET);
+            request.RequestFormat = DataFormat.Json;
             request.AddHeader(APIKeyParameterName, APIKey);
+            if (startDate != null)
+            {
+                request.AddHeader("start", startDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+            if (endDate != null)
+            {
+                request.AddHeader("end", endDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+
             IRestResponse response = client.Execute(request);
 
-            return JsonConvert.DeserializeObject<PricingDTO>(response.Content);
+            return JsonConvert.DeserializeObject<IEnumerable<OrderDTO>>(response.Content);
         }
 
         public OrderDTO CreateCashOrder(CashOrderRequest cashOrderRequest)
@@ -88,6 +130,18 @@ namespace PanxoraCSharpClient
             return JsonConvert.DeserializeObject<OrderDTO>(response.Content);
         }
 
+        public void DeleteCashOrder(string orderRef)
+        {
+            var request = new RestRequest("cash-orders/" + orderRef, Method.DELETE);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddHeader("orderRef", orderRef);
+
+            IRestResponse response = client.Execute(request);
+
+            return;
+        }
+        
         public IEnumerable<TradeDTO> GetTrades()
         {
             var request = new RestRequest("cash-trades", Method.GET);
@@ -97,6 +151,255 @@ namespace PanxoraCSharpClient
             IRestResponse response = client.Execute(request);
 
             return JsonConvert.DeserializeObject<IEnumerable<TradeDTO>>(response.Content);
+        }
+
+        public TradeDTO GetTrade(string tradeRef)
+        {
+            var request = new RestRequest("cash-trades/" + tradeRef, Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddHeader("tradeRef ", tradeRef);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<TradeDTO>(response.Content);
+        }
+
+        public IEnumerable<OrderDTO> ListMarginOrders(DateTime? startDate, DateTime? endDate)
+        {
+            var request = new RestRequest("margin-orders", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            if (startDate != null)
+            {
+                request.AddHeader("start", startDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+            if (endDate != null)
+            {
+                request.AddHeader("end", endDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<OrderDTO>>(response.Content);
+        }
+
+        public OrderDTO CreateMarginOrder(MarginOrderRequestDTO marginOrderRequest)
+        {
+            var request = new RestRequest("cash-orders", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddJsonBody(JsonConvert.SerializeObject(marginOrderRequest));
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<OrderDTO>(response.Content);
+        }
+
+        public OrderDTO GetMarginOrder(string orderRef)
+        {
+            var request = new RestRequest("margin-orders/" + orderRef, Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddHeader("orderRef", orderRef);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<OrderDTO>(response.Content);
+        }
+
+        public void DeleteMarginOrder(string orderRef)
+        {
+            var request = new RestRequest("margin-orders/" + orderRef, Method.DELETE);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddHeader("orderRef", orderRef);
+
+            IRestResponse response = client.Execute(request);
+
+            return;
+        }
+
+        public IEnumerable<MarginPositionDTO> ListMarginPositions()
+        {
+            var request = new RestRequest("margin-positions", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+        
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<MarginPositionDTO>>(response.Content);
+        }
+
+        public IEnumerable<TradeDTO> ListMarginTrades(DateTime? startDate, DateTime? endDate)
+        {
+            var request = new RestRequest("margin-trades", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            if (startDate != null)
+            {
+                request.AddHeader("start", startDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+            if (endDate != null)
+            {
+                request.AddHeader("end", endDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<TradeDTO>>(response.Content);
+        }
+
+        public TradeDTO GetMarginTrade(string tradeRef)
+        {
+            var request = new RestRequest("margin-trades/" + tradeRef, Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddHeader("tradeRef", tradeRef);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<TradeDTO>(response.Content);
+        }
+
+        public IEnumerable<MarketDTO> ListMarkets()
+        {
+            var request = new RestRequest("markets", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<MarketDTO>>(response.Content);
+        }
+
+        public MarketDTO GetMarketDetails(string market)
+        {
+            var request = new RestRequest(string.Format("markets/{0}", market), Method.GET);
+            request.AddHeader(APIKeyParameterName, APIKey);
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<MarketDTO>(response.Content);
+        }
+
+        public IEnumerable<MarketDTO> ListMarketChartData(string market, DateTime? startDate, DateTime? endDate, string aggregationLevel, int? rows)
+        {
+            var request = new RestRequest(string.Format("markets/{0}/chart-data", market), Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            if (startDate != null)
+            {
+                request.AddHeader("start", startDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+            if (endDate != null)
+            {
+                request.AddHeader("end", endDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+            request.AddHeader("aggregationLevel", aggregationLevel);
+            if (rows != null)
+            {
+                request.AddHeader("rows", rows.Value.ToString());
+            }
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<MarketDTO>>(response.Content);
+        }
+
+        public IEnumerable<MarketDepthDTO> ListMarketDepth(string market)
+        {
+            var request = new RestRequest(string.Format("markets/{0}/depth", market), Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<MarketDepthDTO>>(response.Content);
+        }
+
+        public PricingDTO GetMarketPrices(string market)
+        {
+            var request = new RestRequest(string.Format("markets/{0}/prices", market), Method.GET);
+            request.AddHeader(APIKeyParameterName, APIKey);
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<PricingDTO>(response.Content);
+        }
+
+        public IEnumerable<TradeHistoryDTO> ListTradeHistory(string market, DateTime? startDate, DateTime? endDate)
+        {
+            var request = new RestRequest(string.Format("trade-history/{0}", market), Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            if (startDate != null)
+            {
+                request.AddHeader("start", startDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+            if (endDate != null)
+            {
+                request.AddHeader("end", endDate.Value.ToString("yyyy-MM-ddTHH:mm:ss"));
+            }
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<TradeHistoryDTO>>(response.Content);
+        }
+
+        public IEnumerable<TradeHistoryDTO> ListTimeSales(string market)
+        {
+            var request = new RestRequest(string.Format("trade-history/{0}/time-sales", market), Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<TradeHistoryDTO>>(response.Content);
+        }
+
+        public IEnumerable<WatchlistDTO> ListWatchLists()
+        {
+            var request = new RestRequest("watchlists", Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<WatchlistDTO>>(response.Content);
+        }
+
+        public IEnumerable<PricingDTO> ListGlobalWatchListPrices(string globalWatchlistName)
+        {
+            var request = new RestRequest(string.Format("watchlists/{0}", globalWatchlistName), Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddHeader("globalWatchlistName  ", globalWatchlistName);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<PricingDTO>>(response.Content);
+        }
+
+        public IEnumerable<PricingDTO> ListWatchListPrices(string watchlistName)
+        {
+            var request = new RestRequest(string.Format("watchlists/{0}", watchlistName), Method.GET);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddHeader("watchlistName ", watchlistName);
+
+            IRestResponse response = client.Execute(request);
+
+            return JsonConvert.DeserializeObject<IEnumerable<PricingDTO>>(response.Content);
+        }
+
+        public void DeleteUserWatchList(string watchlistName)
+        {
+            var request = new RestRequest(string.Format("watchlists/{0}", watchlistName), Method.DELETE);
+            request.RequestFormat = DataFormat.Json;
+            request.AddHeader(APIKeyParameterName, APIKey);
+            request.AddHeader("watchlistName ", watchlistName);
+
+            IRestResponse response = client.Execute(request);
+
+            return;
         }
     }
 }
